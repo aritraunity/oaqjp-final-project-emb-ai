fbbc41e474c9f11a541990cd0092e5813f51a406
Aritra Mukherjee <gamedev.aritra@gmail.com>
2026-04-26 08:50:33 -0400

    Improved Static Code Analysis Score

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+Executing this function initiates the application of sentiment
+analysis to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
 #Import Flask and Dependencies
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
@@ -13,7 +18,7 @@ def sent_emotion_detector():
     """
     text_to_analyze = request.args.get("textToAnalyze")
     result = emotion_detector(text_to_analyze)
-    if result['anger'] == None:
+    if result['anger'] is None:
         return "Invalid text! Please try again!"
 
     anger = result['anger']
@@ -22,8 +27,10 @@ def sent_emotion_detector():
     joy = result['joy']
     sadness = result['sadness']
     dominant_emotion = result['dominant_emotion']
-    return f"For the given statement, the system response is 'anger':{anger}, 'disgust':{disgust}, 'fear':{fear}, 'joy':{joy} and 'sadness':{sadness}. The dominant emotion is {dominant_emotion}"
-
+    out = f"For the given statement, the system response is 'anger':{anger},\
+    'disgust':{disgust}, 'fear':{fear}, 'joy':{joy} and 'sadness':{sadness}.\
+    The dominant emotion is {dominant_emotion}"
+    return out
 @app.route("/")
 def render_index_page():
     """
@@ -33,7 +40,4 @@ def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    """
-    This functions executes the flask app and deploys it on localhost:5000
-    """
     app.run(host='0.0.0.0', port=5000)
